433e231deaf5c0965ad675475ed175d56c60722b
Artem Shuvaev <shuvaev13@mail.ru>
2021-02-07 18:38:37 +0500

    fix: heroku app

--- main.go
+++ main.go
@@ -1,28 +1,35 @@
 package main
+
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
- 
+
 func productsHandler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id := vars["id"]
-    response := fmt.Sprintf("id=%s", id)
-    fmt.Fprint(w, response)
+	vars := mux.Vars(r)
+	id := vars["id"]
+	response := fmt.Sprintf("id=%s", id)
+	fmt.Fprint(w, response)
 }
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Index Page")
+	fmt.Fprint(w, "Index Page")
 }
- 
+
 func main() {
-      
-    router := mux.NewRouter()
-    router.HandleFunc("/products/{id:[0-9]+}", productsHandler).Methods("GET")
-    router.HandleFunc("/articles/{id:[0-9]+}", productsHandler).Methods("POST")
-    router.HandleFunc("/", indexHandler)
-    http.Handle("/",router)
- 
-    fmt.Println("Server is listening...")
-    http.ListenAndServe(":8181", nil)
+	port := os.Getenv("PORT")
+    // if port == nil {
+    //     port = "8181"
+    // }
+
+	router := mux.NewRouter()
+	router.HandleFunc("/products/{id:[0-9]+}", productsHandler).Methods("GET")
+	router.HandleFunc("/articles/{id:[0-9]+}", productsHandler).Methods("POST")
+	router.HandleFunc("/", indexHandler)
+	http.Handle("/", router)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":"+port, nil)
 }
